465fe3dc48f9b32f15cfafb6efbcfc71103fe928
Andrii Sheremeta <162326622+m9ta92@users.noreply.github.com>
2024-07-23 21:00:44 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,35 +1,33 @@
 
 
-const inputRef = document.querySelector("#controls input");
-const createBtnRef = document.querySelector("button[data-create]");
-const destroyBtnRef = document.querySelector("button[data-destroy]");
-const boxesRef = document.querySelector("#boxes");
+const input = document.querySelector("#controls input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
 
-createBtnRef.addEventListener("click", () => {
-  const amount = parseInt(inputRef.value);
+createBtn.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+
   if (amount >= 1 && amount <= 100) {
-    createBoxes(amount);
-    inputRef.value = "";
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement("div");
+      box.style.width = `${30 + i * 10}px`;
+      box.style.height = `${30 + i * 10}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      fragment.appendChild(box);
+      box.innerHTML = "";
+    }
+
+    boxes.appendChild(fragment);
   }
 });
 
-destroyBtnRef.addEventListener("click", destroyBoxes);
-
-function createBoxes(amount) {
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement("div");
-    box.style.width = `${30 + i * 10}px`;
-    box.style.height = `${30 + i * 10}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    boxesRef.appendChild(box);
-    box.innerHTML = "";
-  }
-}
-
+destroyBtn.addEventListener("click", destroyBoxes);
 function destroyBoxes() {
-  boxesRef.innerHTML = "";
+  boxes.innerHTML = "";
 }
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
